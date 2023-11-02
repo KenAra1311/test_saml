32b848a7afcd20ca7d72e4d87ffc1bf24ec0d160
KenAra <louis.9814@gmail.com>
2023-11-02 20:17:58 +0900

    ログイン画面追加 - 3

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,27 +1,31 @@
-/** @format */
-
-import { initializeApp } from "firebase/app";
-import { getAuth, SAMLAuthProvider, signInWithPopup } from "firebase/auth";
+import firebase from "firebase/app";
+import "firebase/auth";
 
 export default function Home() {
-  const provider = new SAMLAuthProvider("saml.プロバイダー名");
+  const firebaseConfig = {
+    apiKey: process.env.REACT_APP_FIREBASE_KEY,
+    authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+    appId: process.env.REACT_APP_FIREBASE_APP_ID,
+    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  };
+
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+
+  const auth = firebase.auth();
+  const provider = new firebase.auth.SAMLAuthProvider("saml.test_saml");
 
   const onClickSignIn = async () => {
     try {
-      const result = await signInWithPopup(auth, provider);
+      const result = await auth.signInWithPopup(provider);
       console.log("result", result);
     } catch (e) {
       console.error(e);
     }
   };
-
-  const config = {
-    apiKey: "firebaseのAPIキー",
-    authDomain: "firebaseのドメイン",
-  };
-
-  const app = initializeApp(config);
-  const auth = getAuth(app);
 
   return (
     <>
