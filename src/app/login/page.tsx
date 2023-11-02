e12daab01c869ba79529f634c98e76a8fa2868ff
KenAra <louis.9814@gmail.com>
2023-11-02 15:28:30 +0900

    ログイン画面追加

---
+++ src/app/login/page.tsx
@@ -0,0 +1,39 @@
+/** @format */
+
+import { initializeApp } from "firebase/app";
+import {
+  getAuth,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  SAMLAuthProvider,
+  signInWithPopup,
+} from "firebase/auth";
+
+export default function Home() {
+  const provider = new SAMLAuthProvider("saml.プロバイダー名");
+
+  const onClickSignIn = async () => {
+    try {
+      const result = await signInWithPopup(auth, provider);
+      console.log("result", result);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const config = {
+    apiKey: "firebaseのAPIキー",
+    authDomain: "firebaseのドメイン",
+  };
+
+  const app = initializeApp(config);
+  const auth = getAuth(app);
+
+  return (
+    <>
+      <div>Identity Platform Quickstart</div>
+      <div id="message">Loading...</div>
+      <button onClick={onClickSignIn}>sign in</button>
+    </>
+  );
+}
